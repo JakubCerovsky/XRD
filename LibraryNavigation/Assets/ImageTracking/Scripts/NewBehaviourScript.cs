e213409428dab6cfcdb0a875e56584cfaa4cbd55
Cosmin Gabriel Demian <cosmindemian49@gmail.com>
2025-10-02 14:44:33 +0200

    VIA ui changes

---
+++ LibraryNavigation/Assets/ImageTracking/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ImageCubeSpawner : MonoBehaviour
+{
+    [SerializeField] ARTrackedImageManager trackedImageManager;
+    [SerializeField] GameObject cubePrefab;
+
+    // Keep one cube per reference image name
+    Dictionary<string, GameObject> spawned = new Dictionary<string, GameObject>();
+
+    void OnEnable()  => trackedImageManager.trackedImagesChanged += OnChanged;
+    void OnDisable() => trackedImageManager.trackedImagesChanged -= OnChanged;
+
+    void OnChanged(ARTrackedImagesChangedEventArgs args)
+    {
+        foreach (var img in args.added)   UpdateFor(img);
+        foreach (var img in args.updated) UpdateFor(img);
+
+        foreach (var img in args.removed)
+        {
+            var key = img.referenceImage.name;
+            if (spawned.TryGetValue(key, out var go))
+            {
+                Destroy(go);
+                spawned.Remove(key);
+            }
+        }
+    }
+
+    void UpdateFor(ARTrackedImage img)
+    {
+        var key = img.referenceImage.name;
+
+        // Create cube if we don't have one yet
+        if (!spawned.TryGetValue(key, out var go))
+        {
+            go = Instantiate(cubePrefab, img.transform);
+            go.transform.localPosition = Vector3.zero;            // sit on the image center
+            go.transform.localRotation = Quaternion.identity;     // align with image
+            spawned[key] = go;
+        }
+
+        // Show only while tracking is good
+        bool visible = img.trackingState == TrackingState.Tracking;
+        if (go.activeSelf != visible) go.SetActive(visible);
+    }
+}
